4c326d899a92ba266e91577384c810cba2b8b9b1
Manish <manishju22@gmail.com>
2023-09-24 17:12:16 +0530

    Schdule meeting for today

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TimepickerComponent } from './timepicker/timepicker.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimepickerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
